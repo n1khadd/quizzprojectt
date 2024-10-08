1f33775a97e4210f5632f7aed5fb1c9ecf82a26b
n1khadd <mirzeyevnihad60@gmail.com>
2024-10-08 20:13:45 +0400

    yeni sual yaratmaq funksiyasi

--- quizzprojectt/quizzprojectt.cpp
+++ quizzprojectt/quizzprojectt.cpp
@@ -10,3 +10,8 @@ string cavablar[MAX_SUALLAR][3];
 char duzgunCavablar[MAX_SUALLAR];
 int sualSayi = 0;
 
+void yeniSualYarat() {
+    if (sualSayi >= MAX_SUALLAR) {
+        cout << "Maksimum sual sayına çatdınız!\n";
+        return;
+    }
