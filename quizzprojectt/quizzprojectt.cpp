2091d4fbc94ae7af99d3beebfc79e99b23f180b0
n1khadd <mirzeyevnihad60@gmail.com>
2024-10-08 20:12:51 +0400

    suallarin strukturu

--- quizzprojectt/quizzprojectt.cpp
+++ quizzprojectt/quizzprojectt.cpp
@@ -1,1 +1,12 @@
-﻿
+﻿#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+using namespace std;
+
+const int MAX_SUALLAR = 10;
+string suallar[MAX_SUALLAR];
+string cavablar[MAX_SUALLAR][3];
+char duzgunCavablar[MAX_SUALLAR];
+int sualSayi = 0;
+
